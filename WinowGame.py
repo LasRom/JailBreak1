c014de52c36d6e44b5c72efd7a5ba9b1890751bb
LasRom <83460902+LasRom@users.noreply.github.com>
2021-12-18 19:08:33 +0300

    Game 16.12.21 21:32

--- WinowGame.py
+++ WinowGame.py
@@ -5,7 +5,6 @@ import pygame
 
 pygame.init()
 size = width, height = 1000, 700
-FPS = 60
 screen = pygame.display.set_mode(size)
 
 
@@ -18,33 +17,65 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
+class Player(pygame.sprite.Sprite):
+    image = load_image("stop_zakl.png")
+    image_right = load_image("beg_zakl.png")
+    image_stop = load_image("stop_zakl.png")
+    def __init__(self, group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно !!!
+        super().__init__(group)
+        self.image = Player.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 620
 
+    def update(self):
+        if up:
+            if self.rect.y - 6 > 0:
+                self.rect.y -= 6
+        elif right:
+            if self.rect.x + 6 < 965:
+                self.image = self.image_right
+                self.rect.x += 6
+        elif left:
+            if self.rect.x - 6 >= 0:
+                self.rect.x -= 6
+        else:
+            self.image = self.image_stop
+
+up = False
+left = False
+right = False
+player_sprites = pygame.sprite.Group()
+Player(player_sprites)
+
+RUN = True
+FPS = 60
 clock = pygame.time.Clock()
-x = 0
-y = 0
-running = True
-while running:
-    screen.fill([255, 255, 255])
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                if x - 10 >= 0:
-                    x -= 10
-                    screen.blit(load_image('beg_zakl.png'), (x, y))
-            if event.key == pygame.K_RIGHT:
-                x += 10
-                screen.blit(load_image('beg_zakl.png'), (x, y))
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                if x - 10 >= 0:
-                    x -= 10
-                    screen.blit(load_image('beg_zakl.png'), (x, y))
-            if event.key == pygame.K_RIGHT:
-                x += 10
-                screen.blit(load_image('beg_zakl.png'), (x, y))
 
+
+while RUN:
+    clock.tick(FPS)
+    for e in pygame.event.get():  # Обрабатываем события
+        if e.type == pygame.QUIT:
+            RUN = False
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_UP:
+            up = True
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_LEFT:
+            left = True
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
+            right = True
+
+        if e.type == pygame.KEYUP and e.key == pygame.K_UP:
+            up = False
+        if e.type == pygame.KEYUP and e.key == pygame.K_RIGHT:
+            right = False
+        if e.type == pygame.KEYUP and e.key == pygame.K_LEFT:
+            left = False
+    screen.fill([255, 255, 255])
+    player_sprites.draw(screen)
+    player_sprites.update()
     pygame.display.flip()
 
 pygame.quit()
