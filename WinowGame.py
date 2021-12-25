542122b4ffca0bef6ce4089aac5b11c001e5d869
LasRom <83460902+LasRom@users.noreply.github.com>
2021-12-25 16:13:20 +0300

    Game and level 25.12.21 16:13

--- WinowGame.py
+++ WinowGame.py
@@ -11,6 +11,7 @@ UP = False  # проверка на движение вверх
 LEFT = False  # проверка движения в лево
 RIGHT = False  # проверка движения в право
 DOWN = False  # проверка движения в низ
+HERO = None
 PLATFORM_WIDTH = 32  # размеры платформы и лестницы
 PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = (128, 128, 128)  # цвет платформы
@@ -95,20 +96,26 @@ class Player(pygame.sprite.Sprite):
     image_right = load_image("beg_zakl.png")
     image_stop = load_image("stop_zakl.png")
 
-    def __init__(self):
+    def __init__(self, x, y):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
         # Это очень важно !!!
         super().__init__(all_sprites)
         self.image = Player.image
+        self.speed = 3
         self.rect = self.image.get_rect()
-        self.rect.x = 50
-        self.rect.y = 50
+        self.rect.x = x
+        self.rect.y = y
         self.mask = pygame.mask.from_surface(self.image)
         self.flag_g = True  # если в воздухе
         self.flag_st = False  # если на лестнице
+        # дальше идут доп проверки дабы исправить некоторые баги
         self.flag_st_dop = True
 
     def update(self):
+        # проверяю не дошел ли герой до двери
+        for el in door_sprite:
+            if pygame.sprite.collide_mask(self, el):
+                terminate()
         # проверяю не в воздухе ли персонаж
         for el in platforms_and_stairs:
             if not pygame.sprite.collide_mask(self, el):
@@ -126,25 +133,25 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.flag_st = True
                 break
-        # доп проверка для исправления одного из багов
+        # доп проверка для исправления одного из багов(игрок при спуске по лестнице проваливался в платформу)
         for el in platforms:
             if pygame.sprite.collide_mask(self, el):
                 self.flag_st_dop = False
                 break
             else:
                 self.flag_st_dop = True
-        # если в воздухе то он равномерно падает
+        # движение вверх, вправо, влево, вниз
         if UP and self.flag_st:
             self.rect.y -= 5
             self.image = self.image_stop
-        if RIGHT and self.rect.x + 5 < width - 70:
+        if RIGHT and self.rect.x + self.speed < width - 70:
             self.image = self.image_right
-            self.rect.x += 5
-        if LEFT and self.rect.x - 5 > 30:
-            self.rect.x -= 5
+            self.rect.x += self.speed
+        if LEFT and self.rect.x - self.speed > 33:
+            self.rect.x -= self.speed
             self.image = self.image_stop
         if DOWN and self.flag_st and self.flag_st_dop:
-            self.rect.y += 5
+            self.rect.y += self.speed
             self.image = self.image_stop
         if not (LEFT and UP and RIGHT and DOWN):
             self.image = self.image_stop
@@ -169,6 +176,8 @@ print(level)
 
 
 class Platform(pygame.sprite.Sprite):
+    image_stairs = load_image("Лестница.png")
+
     def __init__(self, x, y, plat="стена"):
         super().__init__(all_sprites)
         if plat == "стена":
@@ -177,8 +186,8 @@ class Platform(pygame.sprite.Sprite):
             self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
             self.mask = pygame.mask.from_surface(self.image)
         elif plat == "лестница":
-            self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-            self.image.fill(Color(STAIRS_COLOR))
+            self.image = Platform.image_stairs
+            self.image = pygame.transform.scale(self.image, (32, 32))
             self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
             self.mask = pygame.mask.from_surface(self.image)
         elif plat == "дверь":
@@ -200,6 +209,9 @@ class Creat_Police(pygame.sprite.Sprite):
         self.flag = True
 
     def update(self):
+        for el in hero_sprite:
+            if pygame.sprite.collide_mask(self, el):
+                terminate()
         if self.x + 3 > width - 70:
             self.flag = False
         elif self.x - 3 < 32:
@@ -210,8 +222,6 @@ class Creat_Police(pygame.sprite.Sprite):
         else:
             self.x -= 1.5
             self.rect = Rect(int(self.x), self.y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
-
-
 
 
 x = y = 0
@@ -234,14 +244,15 @@ for row in level:  # вся строка
         elif col == "P":
             pol = Creat_Police(x, y)
             policemen_sprite.add(pol)
+        elif col == "H":
+            hero = Player(x, y)
+            hero_sprite.add(hero)
+            HERO = hero
         x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
     y += PLATFORM_HEIGHT  # то же самое и с высотой
     x = 0  # на каждой новой строчке начинаем с нуля
 
 # добавляю игрока во все спрайты в конце чтобы он не пропадал за платформами
-hero = Player()
-hero_sprite.add(hero)
-all_sprites.add(hero)
 RUN = True
 while RUN:
     screen.fill([255, 255, 255])
@@ -271,6 +282,8 @@ while RUN:
     all_sprites.update()
     policemen_sprite.draw(screen)
     policemen_sprite.update()
+    hero_sprite.draw(screen)
+    hero_sprite.update()
     pygame.display.flip()
 
 pygame.quit()
